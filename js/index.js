832dd25440f53368e1ea73c3263a4f0407c5b4c0
theoarvin <arvintheo69@gmail.com>
2022-12-28 11:43:13 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,70 @@
+async function getData() {
+    let jsonFile = "../recipes.json";
+    let response = await fetch(jsonFile);
+    let data = await response.json();
+    let {recipes} = await data;
+    return recipes
+}
+
+async function init() {
+    const recipes = await getData()
+    getIngredients(recipes)
+    getAppareils(recipes)
+    getUstensiles(recipes)
+    displayRecipes(recipes)
+}
+init()
+
+function displayRecipes(recipes){
+   const blockRecipes = document.querySelector('.recipes-block');
+   
+   recipes.forEach((recipe) => {
+    
+    const card = document.createElement('div');
+    const cardImg = document.createElement('div');
+
+    const cardDesc = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const span = document.createElement('span')
+
+    const recipeBlock = document.createElement('div');
+    const recipeIngredients = document.createElement('ul');
+    
+    const recipeDesc = document.createElement('p')
+    
+    card.classList.add('card')
+    cardImg.classList.add('card-img')
+    cardDesc.classList.add('card-desc')
+    recipeBlock.classList.add('recipe')
+    recipeIngredients.classList.add('recipe-ingredients')
+    recipeDesc.classList.add('recipe-desc')
+    
+    h2.textContent = recipe.name
+    span.innerHTML = `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18ZM10.5 5H9V11L14.2 14.2L15 12.9L10.5 10.2V5Z" fill="black"/>
+    </svg>
+     ${recipe.time } min`
+    recipeDesc.textContent = recipe.description
+    for( let value of recipe.ingredients){
+        const ingredient = document.createElement('li');
+        if(value.unit){
+            ingredient.innerHTML = `<span>${value.ingredient} :</span> ${value.quantity}  ${value.unit}` 
+        }else{
+            ingredient.innerHTML = value.quantity ? `<span>${value.ingredient} :</span> ${value.quantity}` : value.ingredient
+        }
+        recipeIngredients.appendChild(ingredient)
+    }
+ 
+    card.appendChild(cardImg)
+    card.appendChild(cardDesc)
+    cardDesc.appendChild(h2)
+    cardDesc.appendChild(span)
+    card.appendChild(recipeBlock)
+    recipeBlock.appendChild(recipeIngredients)
+    recipeBlock.appendChild(recipeDesc)
+    blockRecipes.appendChild(card)
+   })
+
+   
+}  
+
---
+++ js/tags.js
@@ -0,0 +1,226 @@
+let filters = document.querySelectorAll('.filter');
+
+filters.forEach((filter) => {
+    filter.childNodes[5].addEventListener('click',() => {
+        const filterTitle = filter.childNodes[1];
+        filterTitle.style.display = filterTitle.style.display === 'none' ? 'block' : 'none' ;
+        const filterSearch = filter.childNodes[3];
+        filterSearch.style.display = filterSearch.style.display === 'block' ? 'none' : 'block' ;
+        const blockList = filter.childNodes[7];
+        blockList.style.display = blockList.style.display === 'flex' ? 'none' : 'flex'; 
+    })
+})
+
+const btnFilters = document.querySelectorAll('.btn-filter')
+btnFilters[0].addEventListener('click', function() {
+filters[0].classList.toggle('filter1-active')
+})
+
+btnFilters[1].addEventListener('click', function() {
+filters[1].classList.toggle('filter2-active')
+})
+
+btnFilters[2].addEventListener('click', function() {
+filters[2].classList.toggle('filter3-active')
+})
+
+
+async function getIngredients(recipes){
+    const array = []
+    recipes.forEach((recipe) => {
+     
+      for( let recip of recipe.ingredients ){
+         if(array.includes(recip.ingredient) === false){
+             array.push(recip.ingredient)
+         }
+        }
+    })
+    // création ingrédients list
+    const blockList = document.createElement('ul');
+    blockList.classList.add('block-list')
+    array.forEach((ingredient) => {
+       const list = document.createElement('li')
+       list.classList.add('list-ingredients')
+       list.textContent = ingredient
+       blockList.appendChild(list)
+    })
+    filters[0].appendChild(blockList)
+    
+    const className = '.block-list'
+    const inputClassName = '.input1'
+    const classNameList = 'list-ingredients'
+    searchTag(array, className, inputClassName, classNameList)
+    displayIngredientsTags()
+ }
+ 
+ 
+ async function getAppareils(recipes){
+     const array = []
+       for( let recip of recipes ){
+         if(array.includes(recip.appliance) === false){
+             array.push(recip.appliance)
+         }
+       }
+        // création Appareils list
+       const blockList = document.createElement('ul');
+       blockList.classList.add('block-list2')
+       array.forEach((appareil) => {
+          const list = document.createElement('li')
+          list.classList.add('list-appareils')
+          list.textContent = appareil
+          blockList.appendChild(list)
+       }) 
+       filters[1].appendChild(blockList) 
+       
+       const className = '.block-list2'
+       const inputClassName = '.input2'
+       const classNameList = 'list-appareils'
+       searchTag(array, className, inputClassName,  classNameList)
+       displayAppareilsTags()
+ }
+ 
+
+ async function getUstensiles(recipes){
+     const array = []
+     recipes.forEach((recipe) => {
+     
+         for( let recip of recipe.ustensils ){
+            if(array.includes(recip) === false){
+                array.push(recip)
+            }
+           }
+       })
+        // création ustensiles list
+       const blockList = document.createElement('ul');
+       blockList.classList.add('block-list3')
+       array.forEach((appareil) => {
+          const list = document.createElement('li')
+          list.classList.add('list-ustensiles')
+          list.textContent = appareil
+          blockList.appendChild(list)
+       }) 
+       filters[2].appendChild(blockList)
+       const className = '.block-list3'
+       const inputClassName = '.input3'
+       const classNameList = 'list-ustensiles'
+       searchTag(array, className, inputClassName, classNameList)
+       displayUstensilesTags()
+ }
+
+
+ function displayIngredientsTags(){
+  const list = document.querySelectorAll('.list-ingredients')
+
+  list.forEach((ingredient) => {
+     ingredient.addEventListener('click', function(){
+          // création tag 
+          const tagBlock = document.querySelector('.tags')
+          const tag = document.createElement('span');
+          
+          for(let value of tagBlock.childNodes){
+            if(value.textContent === ingredient.textContent){
+               return null
+            }
+          }
+          if(tagBlock.childNodes.length + 1 > 3 ){
+            return null
+          }else{
+            tag.classList.add('ingredients-tag',)
+            tag.innerHTML = this.textContent + `<i class="fa-solid fa-xmark close-tag"></i>`;
+            tagBlock.appendChild(tag)
+          }
+          // delete tag with close btn 
+          tag.childNodes[1].addEventListener('click', function(){
+             tag.remove(this)
+          })
+     })
+  })
+}
+
+function displayAppareilsTags(){
+    const list = document.querySelectorAll('.list-appareils')
+    
+    list.forEach((ingredient) => {
+        
+       ingredient.addEventListener('click', function(){
+          // création tag 
+          const tagBlock = document.querySelector('.tags')
+          const tag = document.createElement('span');
+          
+          for(let value of tagBlock.childNodes){
+            if(value.textContent === ingredient.textContent){
+               return null
+            }
+          }
+        
+          if(tagBlock.childNodes.length + 1 > 3 ){
+            return null
+          }else{ 
+            tag.classList.add('appareils-tag',)
+            tag.innerHTML = this.textContent + `<i class="fa-solid fa-xmark close-tag"></i>`;
+            tagBlock.appendChild(tag)
+          }
+          // delete tag with close btn 
+          tag.childNodes[1].addEventListener('click', function(){
+             tag.remove(this)
+          })
+       })
+    })
+  }
+
+ function displayUstensilesTags(){
+    const list = document.querySelectorAll('.list-ustensiles');
+    list.forEach((ingredient) => {
+       ingredient.addEventListener('click', function(){
+          // création tag 
+          const tagBlock = document.querySelector('.tags')
+          const tag = document.createElement('span');
+          
+          for(let value of tagBlock.childNodes){
+            if(value.textContent === ingredient.textContent){
+               return null
+            }
+          }
+          if(tagBlock.childNodes.length + 1 > 3 ){
+            return null
+          }else{  
+            tag.classList.add('ustensiles-tag',)
+            tag.innerHTML = this.textContent + `<i class="fa-solid fa-xmark close-tag"></i>`;
+            tagBlock.appendChild(tag)
+          }
+          // delete tag with close btn 
+          tag.childNodes[1].addEventListener('click', function(){
+             tag.remove(this)
+          })
+         
+       })
+    })
+}
+
+function searchTag(array,className,input,classNameList){
+   const inputvalue = document.querySelectorAll(input);
+  
+  
+     inputvalue.forEach((value) => {
+        value.addEventListener('input', (e) => {;
+       
+            const searchedString = e.target.value.toLowerCase()
+            const filteredArr = array.filter(el => el.toLowerCase().includes(searchedString))
+            let blockList = document.querySelector(className);
+            blockList.innerHTML = ""
+            
+            filteredArr.forEach((ingredient) => {
+                const list = document.createElement('li')
+                list.classList.add(classNameList)
+                list.textContent = ingredient
+                blockList.appendChild(list)
+            })
+            displayIngredientsTags()
+            displayUstensilesTags()
+            displayAppareilsTags() 
+         })
+     })  
+}
+
+
+
